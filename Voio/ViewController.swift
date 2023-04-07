d8b1146088f053737e4badef60911e02ec14ec04
Vladimir Lukyanenko <>
2023-04-07 17:18:57 +0300

    first commit

---
+++ Voio/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Voio
+//
+//  Created by Vladimir Lukyanenko on 07.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+
+}
+
